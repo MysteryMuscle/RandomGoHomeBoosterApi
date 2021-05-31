8e49b6cc5213d7b270ed9c5022833e8e47e0440b
pjc1991 <whdekf@naver.com>
2021-05-31 20:50:37 +0900

    Spring Init

---
+++ src/main/java/org/mystery_muscle/random_gohome_booster/RandomGohomeBoosterApplication.java
@@ -0,0 +1,13 @@
+package org.mystery_muscle.random_gohome_booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomGohomeBoosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomGohomeBoosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mystery_muscle/random_gohome_booster/RandomGohomeBoosterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mystery_muscle.random_gohome_booster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomGohomeBoosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
