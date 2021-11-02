c0a8e870131b2bbd53280f149ff9bab769eab983
JC Park <whdekf@gmail.com>
2021-11-02 23:59:14 +0900

    refactor : CurrentUser CustomAnnotation

---
+++ src/main/java/org/mystery_muscle/random_gohome_booster/configuration/CustomHandlerMethodArgumentResolver.java
@@ -0,0 +1,37 @@
+package org.mystery_muscle.random_gohome_booster.configuration;
+
+import org.mystery_muscle.random_gohome_booster.annotation.CurrentUser;
+import org.mystery_muscle.random_gohome_booster.domain.Member;
+import org.mystery_muscle.random_gohome_booster.exception.MMException;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebArgumentResolver;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CustomHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(CurrentUser.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        if (parameter.getParameterType().equals(Member.class)) {
+            if (parameter.getParameterAnnotation(CurrentUser.class) != null) {
+                if (request.getSession().getAttribute("currentUser") != null) {
+                    return (Member) request.getAttribute("currentUser");
+                } else {
+                    return null;
+                }
+            }
+        }
+        return WebArgumentResolver.UNRESOLVED;
+    }
+}
--- src/main/java/org/mystery_muscle/random_gohome_booster/controller/MemberUserController.java
+++ src/main/java/org/mystery_muscle/random_gohome_booster/controller/MemberUserController.java
@@ -1,16 +1,23 @@
 package org.mystery_muscle.random_gohome_booster.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.mystery_muscle.random_gohome_booster.annotation.CurrentUser;
 import org.mystery_muscle.random_gohome_booster.domain.Member;
+import org.mystery_muscle.random_gohome_booster.dto.Login;
 import org.mystery_muscle.random_gohome_booster.service.MemberService;
 import org.mystery_muscle.random_gohome_booster.validator.MemberValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,17 +27,47 @@ public class MemberUserController {
     private final MemberValidator memberValidator;
 
     @GetMapping("/member")
-    public String memberInsertGet(@Validated Member member) {
+    public String memberInsertGet(@CurrentUser Member currentUser, @ModelAttribute Member member, Model model) {
+        if(currentUser != null){
+            return "redirect:/";
+        }
         return "/member/form";
     }
 
     @PostMapping("/member")
     @ResponseStatus(HttpStatus.CREATED)
-    public String memberInsertPost(Member member, BindingResult bindingResult) {
+    public String memberInsertPost(@ModelAttribute Member member, BindingResult bindingResult, Model model) {
         memberValidator.validate(member, bindingResult);
         if (bindingResult.hasErrors()) {
             return "/member/form";
         }
+        member = memberService.insertMember(member);
+        return "redirect:/member/login";
+    }
+
+    @GetMapping("/member/login")
+    public String memberLoginGet(@CurrentUser Member currentUser, @ModelAttribute Login login, Model model){
+        if(currentUser != null){
+            return "redirect:/";
+        }
+
+        return "/member/login";
+    }
+
+    @PostMapping("/member/login")
+    public String memberLoginPost(@CurrentUser Member currentUser,@ModelAttribute Login login, Model model, HttpServletRequest request){
+
+        if(currentUser != null){
+            return "redirect:/";
+        }
+
+        Member member = memberService.login(login);
+
+        if(member == null){
+            return "/member/login";
+        }
+        request.getSession().invalidate();
+        request.getSession().setAttribute("currentUser", member);
         return "redirect:/";
     }
 
