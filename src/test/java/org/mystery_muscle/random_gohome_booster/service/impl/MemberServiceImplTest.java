7b4af7aa8a56258383012da50ce22ddb9143fc1e
JC Park <whdekf@gmail.com>
2021-11-08 22:29:24 +0900

    test : MemberService test implemented

--- src/test/java/org/mystery_muscle/random_gohome_booster/service/impl/MemberServiceImplTest.java
+++ src/test/java/org/mystery_muscle/random_gohome_booster/service/impl/MemberServiceImplTest.java
@@ -1,28 +1,35 @@
 package org.mystery_muscle.random_gohome_booster.service.impl;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mystery_muscle.random_gohome_booster.domain.Member;
 import org.mystery_muscle.random_gohome_booster.repository.MemberRepository;
 import org.mystery_muscle.random_gohome_booster.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 public class MemberServiceImplTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MemberServiceImplTest.class);
 
     @Autowired
     private MemberService memberService;
 
     @Autowired
     private MemberRepository memberRepository;
+
 
     @Test
     public void MemberJoinTest(){
@@ -31,19 +38,35 @@ public class MemberServiceImplTest {
         member.setName("test");
 
         // when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.findOne(saveId);
+        member = memberRepository.save(member);
+        Member findMember = memberRepository.findById(member.getId()).get();
 
         // then
         assertEquals(member, findMember);
     }
 
-    @Test
-    public void MemberDuplicateExceptionTest(){
+    @Test()
+    public void MemberDuplicateExceptionTest() throws IllegalStateException{
         // given
+        Member member = new Member();
+        member.setName("test name1");
+        member.setEmail("test@mail.com");
+        member.setPassword(UUID.randomUUID().toString());
+
+        Member duplicate = new Member();
+        duplicate.setName("test name2");
+        duplicate.setEmail("test@mail.com");
+        duplicate.setPassword(UUID.randomUUID().toString());
 
         // when
+        memberService.insertMember(member);
+        Exception exception = assertThrows(IllegalStateException.class, () ->{
+            memberService.insertMember(duplicate);
+        });
 
         // then
+        if(exception == null || exception.getClass() != IllegalStateException.class){
+            fail("this test is failed!");
+        }
     }
 }
