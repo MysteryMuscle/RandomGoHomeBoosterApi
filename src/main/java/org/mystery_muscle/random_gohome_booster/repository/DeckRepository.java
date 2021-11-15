da8e7b7f0dcf2c751e673a2689b2547a210114b1
JC Park <whdekf@gmail.com>
2021-11-15 22:55:37 +0900

    feat : jpa repository methods added

--- src/main/java/org/mystery_muscle/random_gohome_booster/repository/DeckRepository.java
+++ src/main/java/org/mystery_muscle/random_gohome_booster/repository/DeckRepository.java
@@ -4,6 +4,12 @@ import org.mystery_muscle.random_gohome_booster.domain.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DeckRepository extends JpaRepository<Deck, Long> {
+    
+    Deck findByName(String deckName);
+
+    List<Deck> findAllByOwner(Long userId);
 }
