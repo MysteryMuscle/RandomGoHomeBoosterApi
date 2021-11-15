172ce3c7f7649e9182594fdb0d5eeda4494dedf0
JC Park <whdekf@gmail.com>
2021-11-15 22:55:50 +0900

    feat : basic crud

--- src/main/java/org/mystery_muscle/random_gohome_booster/service/DeckService.java
+++ src/main/java/org/mystery_muscle/random_gohome_booster/service/DeckService.java
@@ -2,9 +2,23 @@ package org.mystery_muscle.random_gohome_booster.service;
 
 import org.mystery_muscle.random_gohome_booster.domain.Deck;
 
+import java.util.List;
+
 public interface DeckService {
 
     public Deck insertDeck(Deck deck);
+
+    public Deck getDeck(Long deckId);
+
+    public Deck updateDeck(Long deckId, String deckName, String deckDescription);
+
+    public void deleteDeck(Long deckId);
+
+    public Deck getDeckByName(String deckName);
+
+    public List<Deck> getDecksByOwner(Long userId);
+
+    public List<Deck> getAllDecks();
 
 
 }
--- src/main/java/org/mystery_muscle/random_gohome_booster/service/impl/DeckServiceImpl.java
+++ src/main/java/org/mystery_muscle/random_gohome_booster/service/impl/DeckServiceImpl.java
@@ -5,6 +5,8 @@ import org.mystery_muscle.random_gohome_booster.repository.DeckRepository;
 import org.mystery_muscle.random_gohome_booster.service.DeckService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DeckServiceImpl implements DeckService {
 
@@ -18,5 +20,41 @@ public class DeckServiceImpl implements DeckService {
     public Deck insertDeck(Deck deck) {
         deck = deckRepository.save(deck);
         return deck;
+    }
+
+    @Override
+    public Deck getDeck(Long deckId) {
+        Deck deck = deckRepository.findById(deckId).orElse(null);
+        return deck;
+    }
+
+    @Override
+    public Deck updateDeck(Long deckId, String deckName, String deckDescription) {
+        Deck deck = deckRepository.findById(deckId).orElse(null);
+        deck.changeDeckInfo(deckName, deckDescription);
+        return deck;
+    }
+
+
+    @Override
+    public void deleteDeck(Long deckId) {
+        deckRepository.deleteById(deckId);
+    }
+
+    @Override
+    public Deck getDeckByName(String deckName) {
+        Deck deck = deckRepository.findByName(deckName);
+        return deck;
+    }
+
+    @Override
+    public List<Deck> getDecksByOwner(Long userId) {
+        List<Deck> decks = deckRepository.findAllByOwner(userId);
+        return decks;
+    }
+
+    @Override
+    public List<Deck> getAllDecks() {
+        return deckRepository.findAll();
     }
 }
