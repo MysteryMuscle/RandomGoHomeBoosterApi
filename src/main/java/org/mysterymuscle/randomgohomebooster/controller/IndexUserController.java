840fc2c112f395a1e6b92aab70347dcdf0be99b9
pjc1991 <whdekf@gmail.com>
2022-10-07 17:59:01 +0900

    FEAT - Hello, World!

--- src/main/java/org/mysterymuscle/randomgohomebooster/controller/IndexUserController.java
+++ src/main/java/org/mysterymuscle/randomgohomebooster/controller/IndexUserController.java
@@ -1,20 +1,24 @@
 package org.mysterymuscle.randomgohomebooster.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexUserController {
 
     @GetMapping("/")
     @ResponseBody
-    public String index(Model model
-    , SecurityContext securityContext) {
+    public String index(HttpServletRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        return "security context: " + securityContext.getAuthentication().getPrincipal();
+        return "security context: " + authentication;
     }
 
 }
